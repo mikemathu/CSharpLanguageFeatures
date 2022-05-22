3488a81b151674924a584267ac4b76c5eb47db16
Michael Mathu <mathumichael17@gmail.com>
2022-05-22 19:54:23 +0300

    populating with dictionary initialization

--- WorkingWithDictionary/Program.cs
+++ WorkingWithDictionary/Program.cs
@@ -36,7 +36,18 @@ namespace WorkingWithDictionary
              };
             // Get Lisa.
             Person lisa = peopleB["Lisa"];
-            Console.WriteLine(lisa);
+           Console.WriteLine(lisa);
+
+
+
+            //Populating with dictionary initialization syntax
+            Dictionary<string, Person> peopleC = new Dictionary<string, Person>()
+            {
+                ["Homer"] = new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 },
+                ["Marge"] = new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 },
+                ["Lisa"] = new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 }
+            };
+
         }
 
 
