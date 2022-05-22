c52eae46b96164ebd9456a0ac920434d2cbf796b
Michael Mathu <mathumichael17@gmail.com>
2022-05-22 15:34:10 +0300

    using string.Format method to compose strings that contain data values

---
+++ StringInterpolation/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInterpolation
+{
+    internal class Person
+    {
+        private string name;
+        private string gender;
+        private int age;
+
+      /*  public Person(string name, string gender, int age)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+        }*/
+
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringInterpolation;
+
+namespace StringInterpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Mmike",
+                Gender = "Male",
+                Age = 23
+            };
+
+            string output = string.Format("Name: {0}, Gender: {1}, Age: {2} ", person.Name, person.Gender, person.Age);
+
+            Console.WriteLine(output);
+
+            Console.WriteLine("nAME: {0}, gENDER: {1}, aGE: {2}", person.Name, person.Gender, person.Age);
+        }
+    }
+}
