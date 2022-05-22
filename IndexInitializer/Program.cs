a9e462b42a563589e74f499eeba0521308e6114a
Michael Mathu <mathumichael17@gmail.com>
2022-05-22 20:35:28 +0300

    initializing using collection initializer syntax

--- IndexInitializer/Program.cs
+++ IndexInitializer/Program.cs
@@ -11,10 +11,19 @@ namespace UsingIndexInititalizer
     {
         static void Main(string[] args)
         {
+            /*Initializing a dictionary*//*
             Dictionary<string, Product> products = new Dictionary<string, Product> {
-             { "Kayak", new Product { Name = "Kayak", Price = 275M } },
-             { "Lifejacket", new Product{ Name = "Lifejacket", Price = 48.95M } }
-             };
+                 { "Kayak", new Product { Name = "Kayak", Price = 275M } },
+                 { "Lifejacket", new Product{ Name = "Lifejacket", Price = 48.95M } }
+             };*/
+
+
+            /*Initializing using Collection Initializer syntax*/
+            Dictionary<string, Product> products = new Dictionary<string, Product>
+            {
+                ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+                ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
+            };
 
         }
     }
