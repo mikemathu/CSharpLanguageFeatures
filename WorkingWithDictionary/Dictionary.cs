9411a71be92948b8e926b7ef8e0ce803ace65504
Michael Mathu <mathumichael17@gmail.com>
2022-05-22 19:50:57 +0300

    populating using Add() method

---
+++ WorkingWithDictionary/Dictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithDictionary
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ WorkingWithDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkingWithDictionary;
+
+namespace WorkingWithDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        private static void UseDictionry()
+        {
+            // Populate using Add() method
+            Dictionary<string, Person> peopleA = new Dictionary<string, Person>();
+            peopleA.Add("Homer", new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            peopleA.Add("Marge", new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            peopleA.Add("Lisa", new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+            // Get Homer.
+            Person homer = peopleA["Homer"];
+            Console.WriteLine(homer);
+
+
+
+        }
+    }
+}
+
